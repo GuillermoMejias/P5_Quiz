a24177af51778bd88930b4cf36891544100b930d
Guillermo Mejias <g.mejias@alumnos.upm.es>
2018-05-10 13:28:45 +0200

    P5 entera

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const {models} = require('../models');
+const Sequelize = require('Sequelize');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Inicio' });
+});
+
+/* GET credits page. */
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'Credits' });
+});
+
+/* GET quizzes page. */
+router.get('/quizzes',  function(req, res, next) {
+	models.quiz.findAll().
+	then( quizzes=> {
+		res.render('quizzes', {quizzes});	
+	})
+	.catch(Sequelize.ValidationError, error => {
+		
+	})
+	.catch(error => {
+	});
+	
+});
+
+module.exports = router;
